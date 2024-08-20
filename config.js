b5ef5560b7989d3d885e274add5aee668594d1ab
Dony-uz <mohammedaminziad167@gmail.com>
2024-08-20 14:35:52 +0000

    Update config.js

--- config.js
+++ config.js
@@ -23,14 +23,14 @@ global.location = '1'; // Replace with your location panel
 //===========================//
 
 global.prefa = ['', '!', '.', ',', '🐤', '🗿'];
-global.owner = ['2348133729715'];
-global.ownMain = '2348133729715';
-global.NamaOwner = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Do not change 
+global.owner = ['233249875851'];
+global.ownMain = '233249875851';
+global.NamaOwner = 'HΔSHIRΔMΔ 𝛁1 𝚩𝐔𝐆'; // Do not change 
 global.sessionName = 'session';
 global.connect = true; // Set to false if using QR Code
-global.namabot = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Change if needed
-global.author = '𝚸𝚫𝚻𝚪𝚯𝚴 𝛁1 𝚩𝐔𝐆'; // Change if needed
-global.packname = 'PATRON 🚹'; // Change if needed
+global.namabot = 'HΔSHIRΔMΔ 𝛁1 𝚩𝐔𝐆'; // Change if needed
+global.author = 'HΔSHIRΔMΔ 𝛁1 𝚩𝐔𝐆'; // Change if needed
+global.packname = 'HΔSHIRΔMΔ 🌚'; // Change if needed
 global.url = 'https://t.me/textpatron_bot'; // Do not change
 
 //===========================//
